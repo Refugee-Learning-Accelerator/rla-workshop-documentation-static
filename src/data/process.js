301f1c36b7ffd18cf2ef5563a6d602e7ef8e79a6
Vahid Fazel-Rezai <vahidfazelrezai@gmail.com>
2018-02-08 08:56:18 -0500

    structure into src/dist/assets

---
+++ src/data/process.js
@@ -0,0 +1,25 @@
+var fs = require('fs');
+var path = require('path');
+
+var baseInfo = JSON.parse(fs.readFileSync(path.join(__dirname, '/baseInfo.json'), 'utf8'));
+
+var teams = baseInfo['teams'];
+var folders = baseInfo['folders'];
+var images = {};
+
+var files = fs.readdirSync(path.join(__dirname, '../../assets/img'));
+var clean = (text) => { return text.toLowerCase().replace(/-/g, '').replace(/ /g, ''); }
+
+for (var i = 0; i < teams.length; i++) {
+  images[teams[i]] = files.filter((file) => {
+    return clean(file).startsWith(clean(teams[i]));
+  }).map((file) => {
+    return '/img/' + file;
+  });  
+}
+  
+var data = {}
+data['teams'] = teams;
+data['folders'] = folders;
+data['images'] = images;
+fs.writeFileSync(path.join(__dirname, '/../../dist/data.json'), JSON.stringify(data, null, 4));
